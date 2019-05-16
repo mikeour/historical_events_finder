aee8436fd89e55974bbe602998cf045b72fff7be
Michael Roeslein <michaeljroeslein@gmail.com>
2019-05-16 09:06:59 -0400

    Implemented useCallback in App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, Fragment } from "react";
+import React, { Fragment, useState, useCallback } from "react";
 import Form from "./Form.jsx";
 import Results from "./Results.jsx";
 import MoreButton from "./MoreButton.jsx";
@@ -8,8 +8,9 @@ const App = () => {
   const [results, setResults] = useState("");
   const [nextResults, setNextResults] = useState("");
   const [search, setSearch] = useState("");
-  const [total, setTotal] = useState("");
+  const [totalResults, setTotalResults] = useState("");
   const [page, setPage] = useState(1);
+  const pageIncrement = useCallback(() => page + 1, [page]);
 
   const updateSearch = e => {
     setSearch(e.target.value);
@@ -25,7 +26,7 @@ const App = () => {
         setResults(data);
       })
       .then(() => {
-        setPage(page + 1);
+        setPage(pageIncrement);
         getNextResults();
       })
       .then(() => {
@@ -49,20 +50,21 @@ const App = () => {
   const updateWithNextResults = () => {
     console.log("updateWithNextResults ran");
     setResults(nextResults);
-    setPage(page + 1);
+    setPage(pageIncrement);
     getNextResults();
   };
 
   const getTotalResults = () => {
     axios.get(`/events?q=${search}`).then(({ data }) => {
-      setTotal(data.length);
+      setTotalResults(data.length);
     });
   };
 
   return (
     <Fragment>
+      <h6>Historical Events Finder</h6>
       <Form updateSearch={updateSearch} updateResults={updateResults} />
-      {total && <p>Total search results: {total}</p>}
+      {totalResults && <p>Total search results: {totalResults}</p>}
       <Results results={results} />
       {nextResults && (
         <MoreButton updateWithNextResults={updateWithNextResults} />
--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,19 +1,48 @@
 import React, { Fragment, useState } from "react";
+import Modal from "react-modal";
 
 const Entry = props => {
   const { result } = props;
   const [editedText, setEditedText] = useState(null);
-  const text = editedText || result.description;
+  const [showModal, setShowModal] = useState(false);
+  const [modalText, setModalText] = useState("");
+  const text = modalText || result.description;
+
+  // const editText = () => {
+  //   const userEdit = window.prompt("What would you like to edit?", text);
+  //   setEditedText(userEdit);
+  // };
+
+  const modalStyle = {
+    content: {
+      display: "inline-block",
+      backgroundColor: "goldenrod"
+    }
+  };
 
   const editText = () => {
-    const result = window.prompt("Edited text?", text);
-    setEditedText(result);
+    setShowModal(prevBool => !prevBool);
+  };
+
+  const updateModalText = e => {
+    e.preventDefault();
+    setModalText(e.target.value);
+  };
+
+  const updateText = e => {
+    setEditedText(modalText);
   };
 
   return (
     <Fragment>
       <p>{text}</p>
-      <button onClick={editText}>x</button>
+      <button onClick={editText}>Edit</button>
+      <Modal isOpen={showModal} onRequestClose={editText} style={modalStyle}>
+        <form onSubmit={updateText}>
+          <input type="text" onChange={updateModalText} placeholder={text} />
+        </form>
+        <button onClick={editText}>Yes</button>
+      </Modal>
     </Fragment>
   );
 };
